ae52db0800aa643c1a1b26751c9da01c2dbbf5a3
qww1552 <qww1552>
2021-05-25 20:43:53 +0900

    인수가 배열일 경우

--- 10/src/avg.js
+++ 10/src/avg.js
@@ -1,16 +1,19 @@
-function avg() {
-  let total = 0;
-  let len = 0;
-
-  for (let i of arguments) {
-    const number = Number.parseInt(i);
-    if (Number.isInteger(number)) {
-      total += number;
-      len++;
+function avg(...args) {
+    let total = 0;
+    let len = 0;
+    let numbers;
+    if (Array.isArray(args[0])) {
+        numbers = args[0];
+    } else {
+        numbers = args;
     }
-  }
-
-  return total / len;
+    for (let i of numbers) {
+        const number = Number.parseInt(i);
+        if (Number.isInteger(number)) {
+            total += number;
+            len++;
+        }
+    }
+    return total / len;
 }
-
 export default avg;

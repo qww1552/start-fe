70a69420118c22e5b43678073c47c6c9aea3a374
qww1552 <qww1552>
2021-06-17 14:45:54 +0900

    13

---
+++ 13/react.js
@@ -0,0 +1,11 @@
+// const rootEl = document.getElementById('root');
+// const el = React.createElement(
+//     'div',
+//     {className: 'container'},
+//     'React',
+//   )
+//   ReactDom.render(el,rootEl);
+
+  const rootEl = document.getElementById('root');
+  const el = <div className="container">React</div>;
+  ReactDOM.render(el, rootEl);
